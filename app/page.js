d84290b5060f32bd405d66814a8810707eb5a97c
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-07-02 03:02:19 +0600

    [feature](landing page): landing page created with necessary components and dummy data others

--- app/page.js
+++
@@ -1,8 +0,0 @@
-import { Button } from "@/components/ui/button";
-import { getAllCourses } from "@/queries/course-queries";
-
-export default async function Home() {
-  const courses = await getAllCourses();
-  console.log("courses", courses);
-  return <Button>Button</Button>;
-}
