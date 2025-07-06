42e7e6fd845ad46f1389bb572409e793f0946055
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-07-07 01:17:11 +0600

    [feature](login): login functionality working. necessary action added to make it functional.

--- app/login/_components/SignIn.jsx
+++ app/login/_components/SignIn.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {
@@ -15,8 +17,20 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { ManualLogin } from "@/app/action";
+import { useRouter } from "next/navigation";
 
 export default function SignIn() {
+  const router = useRouter();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const response = await ManualLogin(formData);
+    if (response) router.push("/");
+  };
+
   return (
     <Card className="mx-auto max-w-sm w-full">
       <CardHeader>
@@ -26,45 +40,46 @@ export default function SignIn() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
+        <form action="" onSubmit={onSubmit}>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              {/* <Link href="#" className="ml-auto inline-block text-sm underline">
+              <Input id="password" name="password" type="password" required />
+              {/* <Link href="#" className="ml-auto inline-block text-xs underline">
                 Forgot your password?
               </Link> */}
             </div>
-            <Input id="password" type="password" required />
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
           </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <DropdownMenu>
-            <DropdownMenuTrigger className=" underline">
-              Register
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem asChild>
-                <Link href="/register/student">Student</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link href="/register/instructor">Instructor</Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <DropdownMenu>
+              <DropdownMenuTrigger className=" underline">
+                Register
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem asChild>
+                  <Link href="/register/student">Student</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link href="/register/instructor">Instructor</Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </form>
       </CardContent>
     </Card>
   );
---
+++ auth.js
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { User } from "./models/user-model";
+import bcrypt from "bcrypt";
+
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        if (!credentials) return null;
+        try {
+          const user = await User.findOne({ email: credentials?.email });
+          if (user) {
+            const isMatched = bcrypt.compareSync(
+              credentials?.password,
+              user?.password
+            );
+            if (isMatched) {
+              return user;
+            } else {
+              throw new Error("Invalid credentials!");
+            }
+          } else {
+            throw new Error("User not found!");
+          }
+        } catch (error) {
+          throw new Error(error?.message);
+        }
+      },
+    }),
+  ],
+});
