c9e0270b2d6f9e407f2155a3b43d55c80443e28c
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-07-20 15:31:54 +0600

    [feature](jwt): token generation using jwt for goolge login implimented and commented. waiting for manual login token generation

--- auth.js
+++ auth.js
@@ -5,6 +5,46 @@ import bcrypt from "bcrypt";
 import authConfig from "./auth.config.js"; // make sure this is .js
 import google from "next-auth/providers/google";
 
+async function refreshAccessToken(token) {
+  try {
+    const url =
+      "https://oauth2.googleapis.com/token?" +
+      new URLSearchParams({
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        grant_type: "refresh_token",
+        refresh_token: token.refresh_token,
+      });
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    });
+
+    const refreshedTokens = await response.json();
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      access_token: refreshedTokens?.access_token,
+      expires_in: Date.now() + refreshedTokens?.expires_in * 1000,
+      refresh_token: refreshedTokens?.refresh_token,
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
+
 export const {
   handlers: { GET, POST },
   signIn,
@@ -52,4 +92,25 @@ export const {
       },
     }),
   ],
+  // callbacks: {
+  //   async jwt({ token, user, account }) {
+  //     if (user && account) {
+  //       token.access_token = account?.access_token;
+  //       token.refresh_token = account?.refresh_token;
+  //       token.id_token = account?.id_token;
+  //       token.expires_in = Date.now() + account?.expires_in * 1000;
+  //     }
+
+  //     if (Date.now() > token?.expires_in) {
+  //       return console.log("token expired");
+  //     }
+
+  //     return token;
+  //   },
+
+  //   async session({ session, token }) {
+  //     session.access_token = token?.access_token;
+  //     return session;
+  //   },
+  // },
 });
