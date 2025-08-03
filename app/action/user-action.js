bb3fc6098cddd07ae8d7d4bfdc9b170e054479d7
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-08-03 19:22:40 +0600

    [bugfix](add course): while creating course instructor is was missing. added this on this commit

--- app/action/user-action.js
+++ app/action/user-action.js
@@ -1,8 +1,13 @@
 "use server";
 
+import { auth } from "@/auth";
 import { Course } from "@/models/course-model";
+import { getAUserByEmail } from "@/queries/user-queries";
 
 export async function postACourse(course) {
+  const { user } = await auth();
+  const loggedInUser = await getAUserByEmail(user?.email);
+  course["instructor"] = loggedInUser?.id;
   const response = await Course.create(course);
   return JSON.parse(JSON.stringify(response));
 }
