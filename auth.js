6c1c8c99d4979dca34270e9ee0f77070a4d6f4f4
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-10-08 13:34:00 +0600

    [bugfix](static url): local mongo uri replaced

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/lets-learn");
+const client = new MongoClient(process.env.mongodb_connection_string);
 const db = client.db();
 
 export const auth = betterAuth({
