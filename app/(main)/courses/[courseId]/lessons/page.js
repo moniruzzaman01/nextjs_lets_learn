d729ef1311525e6fbc99bfe03f995edcbcab5aa3
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-12-25 11:33:31 +0600

    [bugfix](video play): private video and invalid lesson/module slug conditionally handled

--- app/(main)/courses/[courseId]/lessons/page.js
+++ app/(main)/courses/[courseId]/lessons/page.js
@@ -7,35 +7,42 @@ import { getALessonByLessonSlugAndModuleSlug } from "@/queries/lesson-queries";
 
 const Course = async ({ params, searchParams }) => {
   const { courseId } = await params;
-  const { lesson: lessonSlug, module: moduleSlug } = await searchParams;
+  const { lesson: lessonSlug, module: moduleSlug, reason } = await searchParams;
   const { modules } = await getACourse(courseId, true);
-  const selectedLesson = (await getALessonByLessonSlugAndModuleSlug(
-    lessonSlug,
-    moduleSlug
-  )) || {
+  let selectedLesson = {
     lessonId: modules[0].lessonIds[0]._id.toString(),
     moduleId: modules[0]._id.toString(),
     video_url: modules[0].lessonIds[0].video_url,
-    // lessonTitle: modules[0].lessonIds[0].title,
-    // moduleTitle: modules[0].title,
   };
+  if (lessonSlug && moduleSlug) {
+    selectedLesson = await getALessonByLessonSlugAndModuleSlug(
+      lessonSlug,
+      moduleSlug
+    );
+  }
 
   return (
-    <div>
-      <div className="flex flex-col max-w-4xl mx-auto pb-20">
-        <div className="p-4 w-full">
-          <VideoPlayer selectedLesson={selectedLesson} courseId={courseId} />
-        </div>
-        <div>
-          <div className="p-4 flex flex-col md:flex-row items-center justify-between">
-            <h2 className="text-2xl font-semibold mb-2 lg:mb-0">
-              Introduction
-            </h2>
-            <Button size="lg">Enroll</Button>
+    <div className="flex flex-col max-w-4xl mx-auto pb-20">
+      <div className="p-4 w-full">
+        {reason == "private-lesson" ? (
+          <div className=" h-[480px] w-full flex justify-center items-center border">
+            <p className="  text-sm text-red-500">This video is private!!!</p>
           </div>
-          <Separator />
-          <VideoDescription />
+        ) : selectedLesson ? (
+          <VideoPlayer selectedLesson={selectedLesson} courseId={courseId} />
+        ) : (
+          <div className=" h-[480px] w-full flex justify-center items-center border">
+            <p className="  text-sm text-red-500">No video found!!!</p>
+          </div>
+        )}
+      </div>
+      <div>
+        <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+          <h2 className="text-2xl font-semibold mb-2 lg:mb-0">Introduction</h2>
+          <Button size="lg">Enroll</Button>
         </div>
+        <Separator />
+        <VideoDescription />
       </div>
     </div>
   );
